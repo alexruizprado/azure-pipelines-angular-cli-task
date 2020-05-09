f1668490988199195d70ccff96958ee0bd4be7ea
Raul Ruiz <alexruizprado@hotmail.com>
2020-05-08 21:12:38 -0400

    increase buffer size on console logs

--- Task1/Task1V1/index.ts
+++ Task1/Task1V1/index.ts
@@ -14,7 +14,8 @@ async function validateNg(folder: string, onSuccess: Function, onError: Function
   exec('npx ng version',
     {
       encoding: "UTF-8",
-      cwd: folder
+      cwd: folder,
+      maxBuffer: 1024 * 10000
     },
     (error, stdout, stderr) => {
       if (error !== null) {
@@ -37,7 +38,8 @@ function execute(command: string, cwd: string, onSuccess: Function, onError: Fun
   return exec(command,
     {
       encoding: "UTF-8",
-      cwd: cwd
+      cwd: cwd,
+      maxBuffer: 1024 * 10000
     }, (error, stdout, stderr) => {
       if (error !== null) {
         console.error(`exec error: ${error}`);
@@ -51,7 +53,7 @@ function execute(command: string, cwd: string, onSuccess: Function, onError: Fun
 async function run() {
   const analyticsDisabled: boolean = tl.getBoolInput('DisableAnalytics', false);
   const telemetry = new AnalyticsService('##{InstrumentationKey}##', analyticsDisabled);
-  telemetry.trackEvent('Angular CLI Started');
+  telemetry.trackEvent('Extension Angular CLI Started...');
   try {
     const command: string | undefined = tl.getInput('command', true);
     const project: string | undefined = tl.getInput('project', false) || '.';
@@ -102,26 +104,31 @@ async function run() {
       console.log(`Executing ng ${custom || command} ${args}`);
       execute(`ng ${custom || command} ${args}`, project, (output: string) => {
         console.log(`Output (ng ${custom || command} ${args}):`, output);
+        telemetry.trackEvent('Extension Angular CLI ended.');
       }, (error: ExecException, stderror: string) => {
         console.error(`There was an error: ${stderror}`, error);
         telemetry.trackException(stderror);
         telemetry.trackExceptionExtended(error);
+        telemetry.trackEvent('Extension Angular CLI ended.');
       }).on('exit', code => {
         if (code == 1) {
           tl.setResult(tl.TaskResult.Failed, `ng ${custom || command} ${args} returned exit code 1`);
         }
+        telemetry.trackEvent('Extension Angular CLI ended with exit code ' + code);
       });
     }, () => {
       const message = 'Angular CLI was not found.\nRemember to perform "npm install" before using the steps of the Angular CLI extension.\nAngular CLI extensions uses @angular/cli package located in the node_modules folder of an Angular application.\nIf you still have issue or doubts you can open a ticket in https://github.com/alexruizprado/azure-pipelines-angular-cli-task under Issues.';
       telemetry.trackException('Angular CLI not found');
       console.error(message);
       tl.setResult(tl.TaskResult.Failed, message);
+      telemetry.trackEvent('Extension Angular CLI ended.');
       return;
     });
   }
   catch (err) {
     telemetry.trackException(err.message);
     tl.setResult(tl.TaskResult.Failed, err.message);
+    telemetry.trackEvent('Extension Angular CLI ended.');
   }
 }
 
